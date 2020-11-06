3b38c1c66586108333e3611b86763e0d1fff0359
Selom Sallas-Mensah <sxs1558@cs.bham.ac.uk>
2020-11-06 13:46:31 +0000

    Updates

---
+++ precache-manifest.2ada039dbee3d38b177c1c1e42649bb3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7a7c87d0f9bccf7d324d91c91ccf105",
+    "url": "/sirsallas/cradlecopy/index.html"
+  },
+  {
+    "revision": "505eaff4e71927ce2a8c",
+    "url": "/sirsallas/cradlecopy/static/js/2.dcc11e7e.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/sirsallas/cradlecopy/static/js/2.dcc11e7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3230d18779ecbb08aaba",
+    "url": "/sirsallas/cradlecopy/static/js/main.66cd030a.chunk.js"
+  },
+  {
+    "revision": "ddbd742f0ff2eafad3e4",
+    "url": "/sirsallas/cradlecopy/static/js/runtime-main.a5487fcc.js"
+  },
+  {
+    "revision": "35a2c8071ab19687a6e10c7e0ac913b3",
+    "url": "/sirsallas/cradlecopy/static/media/cradle2.35a2c807.jpg"
+  },
+  {
+    "revision": "14127d0475a59a4e0db49e65750e865b",
+    "url": "/sirsallas/cradlecopy/static/media/cradle3.14127d04.jpg"
+  },
+  {
+    "revision": "34160689256a0142a77426ef259931ee",
+    "url": "/sirsallas/cradlecopy/static/media/cradle4.34160689.jpg"
+  },
+  {
+    "revision": "76410dc7a8e8b23bf18352f13a15720b",
+    "url": "/sirsallas/cradlecopy/static/media/cradle5.76410dc7.jpeg"
+  }
+]);
